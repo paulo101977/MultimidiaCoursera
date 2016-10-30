3d97434a5501eb06bec82538c2bd5044c6548fac
paulo101977 <paulo10.1977@yahoo.com.br>
2016-10-30 13:12:42 -0200

    Update ListVideoActivity

--- app/src/main/java/com/heroku/a1977/paulo10/multimidiaapp/ListVideoActivity.java
+++ app/src/main/java/com/heroku/a1977/paulo10/multimidiaapp/ListVideoActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class ListVideoActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +19,11 @@ public class ListVideoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btnPlus);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Log.w(TAG,"Simple Click Test");
             }
         });
     }
